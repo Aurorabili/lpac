4ba8d8569599900be0ec4120b2d2a9beac7fbc12
Aurorabili <aurorabili@outlook.com>
2025-08-09 15:56:07 +0800

    Update setup-debian.sh

--- scripts/setup-debian.sh
+++ scripts/setup-debian.sh
@@ -4,7 +4,7 @@ export DEBIAN_FRONTEND=noninteractive
 export DEBIAN_PRIORITY=critical
 
 apt-get -qq -o=Dpkg::Use-Pty=0 update
-apt-get -qq -o=Dpkg::Use-Pty=0 install -y build-essential libpcsclite-dev libcurl4-openssl-dev zip cmake
+apt-get -qq -o=Dpkg::Use-Pty=0 install -y build-essential libpcsclite-dev libcurl4-openssl-dev zip cmake pkg-config libpcsclite-dev
 
 case "${1:-}" in
 mingw)
